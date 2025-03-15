eec77d511e403e714b852489b2cce22e1e61e459
Nadzey <nadiakoluzaeva@gmail.com>
2025-03-15 19:02:40 -0400

    Unit tests add

--- backend/product_service/cdk/jest.config.js
+++ backend/product_service/cdk/jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testEnvironment: "node",
+  setupFiles: ["<rootDir>/test/setupEnv.js"], // Ensure Jest finds setupEnv.js in `cdk/test/`
+  roots: ["<rootDir>/"], // Ensure Jest looks in `cdk`
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testMatch: ["**/*.test.js"], // Ensure Jest finds test files
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
